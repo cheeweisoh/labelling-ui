4560616c39bcdf34ca7bbdf02829684c853de5fa
cheewei <sohcheewei@u.nus.edu>
2025-06-05 00:09:58 +0800

    updated logic to exclude labelled pics

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from PIL import ImageFont
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 import gspread
@@ -14,6 +13,7 @@ from fd_utils import load_model, get_bounding_boxes
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
+
 creds = service_account.Credentials.from_service_account_info(
     st.secrets["labelling_ui_credentials"],
     scopes=[
@@ -21,7 +21,6 @@ creds = service_account.Credentials.from_service_account_info(
         "https://spreadsheets.google.com/feeds",
     ],
 )
-FONT = ImageFont.load_default(size=18)
 
 drive_service = build("drive", "v3", credentials=creds)
 sheet_client = gspread.authorize(creds)
@@ -30,55 +29,62 @@ folder_id = find_folder_id(drive_service, "CS610/Project")
 image_folder_id = find_folder_id(drive_service, "CS610/Project/Images")
 image_files = list_images_in_folder(drive_service, image_folder_id)
 
-output = sheet_client.open_by_key("1GPb0IKBueSGMmpC2rjfCbnvBJlR99JdQK0g_0mGX-bM").sheet1
+output = sheet_client.open_by_key(st.secrets["output_sheet"]["output_sheet_id"]).sheet1
+labelled_images = [x[0] for x in output.get_all_values()[1:]]
+remaining_images = [x for x in image_files if x["name"] not in labelled_images]
 
 face_app = load_model()
 
-if "image_index" not in st.session_state:
-    st.session_state.image_index = 0
-if "annotations" not in st.session_state:
-    st.session_state.annotations = []
-
-
-# Layout
 st.title("Bounding Box Annotation Tool")
 
-current_image_id = image_files[st.session_state.image_index]["id"]
-current_image_name = image_files[st.session_state.image_index]["name"]
-image = get_image_from_drive(drive_service, current_image_id)
+if remaining_images:
+    if "image_index" not in st.session_state:
+        st.session_state.image_index = 0
+    if "annotations" not in st.session_state:
+        st.session_state.annotations = []
 
-vis_img, bbox_list = get_bounding_boxes(image, current_image_name, face_app)
+    current_image_id = remaining_images[st.session_state.image_index]["id"]
+    current_image_name = remaining_images[st.session_state.image_index]["name"]
+    image = get_image_from_drive(drive_service, current_image_id)
 
-st.image(vis_img, caption=current_image_name, use_container_width=True)
+    vis_img, bbox_list = get_bounding_boxes(image, current_image_name, face_app)
 
-st.subheader("Assign labels to the bounding boxes")
-new_labels = []
-for idx, x1, y1, x2, y2 in bbox_list:
-    label = st.text_input(f"Label for box {idx}", key=f"box_{idx}")
-    if label:
-        new_labels.append(
-            [
-                idx,
-                x1,
-                y1,
-                x2,
-                y2,
-                label,
-                datetime.now(ZoneInfo("Asia/Singapore")).strftime("%Y-%m-%d %H:%M:%S"),
-            ]
-        )
+    st.image(vis_img, caption=current_image_name, use_container_width=True)
 
-if st.button("Save Annotations", use_container_width=True):
-    write_label_to_sheet(output, new_labels)
-    st.success("Annotations saved!")
+    st.subheader("Assign labels to the bounding boxes")
+    new_labels = []
+    for idx, x1, y1, x2, y2 in bbox_list:
+        label = st.text_input(f"Label for box {idx}", key=f"box_{idx}")
+        if label:
+            new_labels.append(
+                [
+                    current_image_name,
+                    idx,
+                    x1,
+                    y1,
+                    x2,
+                    y2,
+                    label,
+                    datetime.now(ZoneInfo("Asia/Singapore")).strftime(
+                        "%Y-%m-%d %H:%M:%S"
+                    ),
+                ]
+            )
 
-col1, col2 = st.columns(2)
-with col1:
-    if st.button("Previous", use_container_width=True):
-        prev_image()
-        st.rerun()
+    if st.button("Save Annotations", use_container_width=True):
+        write_label_to_sheet(output, new_labels)
+        st.success("Annotations saved!")
 
-with col2:
-    if st.button("Next", use_container_width=True):
-        next_image(image_files)
-        st.rerun()
+    col1, col2 = st.columns(2)
+    with col1:
+        if st.button("Previous", use_container_width=True):
+            prev_image()
+            st.rerun()
+
+    with col2:
+        if st.button("Next", use_container_width=True):
+            next_image(remaining_images)
+            st.rerun()
+
+else:
+    st.success("🎉 We're Done! 🎉")
